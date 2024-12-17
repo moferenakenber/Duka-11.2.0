600e59e4f3fe4f839bf7aafc06dee19d4ef7103f
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-12-18 02:31:48 +0300

    staff

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -79,6 +79,18 @@
             </x-responsive-nav-link>
         </div>
 
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('User Management') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('customers.index')">
+                {{ __('Customers') }}
+            </x-responsive-nav-link>
+        </div>
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,25 +1,31 @@
 <!-- Sidebar -->
 
-<aside x-data="{ open: false }" class="hidden w-60 bg-gray-800 text-white h-screen flex-shrink-0 sm:flex">
+<aside x-data="{ open: false }" class="hidden w-60 bg-gray-900 text-white h-screen flex-shrink-0 sm:flex items-center justify-center basis-1/4">
 
     <div class="flex flex-col h-full">
-        <div class="flex flex-col">
+        <div class="flex flex-col items-center justify-center">
 
             <!-- Logo -->
             <div class="flex-1 space-y-2 px-4 py-4">
 
-                <div class="flex w-full justify-center items-center px-4 py-3">
+                <div class="px-4 py-4 flex items-center justify-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block h-16 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
             </div>
 
+            <div class="border-t border-gray-700 my-2"></div>    {{-- <--- Optional separator --}}
             <!--Sidebar Links -->
-            <nav class="flex-1 space-y-2 px-4 py-4">
+            <nav class="flex flex-col flex-grow space-y-2 px-4 py-4">
                     <x-sidebar-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-sidebar-link>
+
+
+                    <x-sidebar-link :href="route('customers.index')" :active="request()->routeIs('customers.index')">
+                        {{ __('User Management') }}
                     </x-sidebar-link>
 
                     <x-sidebar-link :href="route('customers.index')" :active="request()->routeIs('customers.index')">
