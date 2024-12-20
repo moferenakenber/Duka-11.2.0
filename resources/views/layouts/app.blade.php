a33e784bf7c0c494036e33f11b346d56a14c5685
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-12-20 04:52:38 +0300

    top nav buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,7 @@
 
                 <!-- Main Content -->
                 <main class="p-4 sm:ml-64">
-                    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-
+                    <div class="p-6 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-20">
                         <!-- Page Heading -->
                         @isset($header)
                             <header class="bg-white dark:bg-gray-800 shadow">
