6914c4d912b5db1a24f73ce7e635ef0ea04209dc
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-12-21 06:49:04 +0300

    add user routes to register which routes to dashboard,

--- resources/views/user_managements/index.blade.php
+++ resources/views/user_managements/index.blade.php
@@ -8,7 +8,7 @@
             </h2>
 
             <!-- Right side: Add Product Button -->
-            <a href="{{ route('customers.create') }}"
+            <a href="{{ route('register') }}"
                class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 {{ __('Add User') }}
             </a>
