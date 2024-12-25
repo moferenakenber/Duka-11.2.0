b405caf74dc5394eff33261a519b4e9434b56fe0
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-12-25 04:50:08 +0300

    cart side bar link and sign out fix in the nav

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\UserManagementController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ItemController;
@@ -7,6 +8,7 @@ use App\Http\Controllers\SaleController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
+use App\Http\Middleware\CheckRole;
 
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +29,8 @@ use Illuminate\Support\Facades\Route;
         })->middleware(['auth', 'verified'])->name('flowbite');
 
 
+
+
         Route::resource('user_managements', UserManagementController::class)
             ->middleware(['auth', 'verified']);
 
@@ -35,6 +39,15 @@ use Illuminate\Support\Facades\Route;
 
         Route::resource('items', ItemController::class)
             ->middleware(['auth', 'verified']);
+
+        Route::resource('carts', CartController::class)
+            ->middleware(['auth', 'verified']);
+
+            // Route::post('cart/{itemId}/add', [CartController::class, 'addToCart'])->name('cart.add')->middleware(['auth', 'verified']);
+
+        // Update your route to accept both GET and POST requests
+        Route::match(['get', 'post'], '/cart/{cart}/add', [CartController::class, 'addItem'])->name('cart.add');
+
 
         Route::resource('products', ProductController::class)
              ->middleware(['auth', 'verified']);
@@ -51,6 +64,24 @@ use Illuminate\Support\Facades\Route;
             Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
             Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
         });
+
+
+        // Route::middleware('CheckRole')->group(function () {
+        //     Route::get('/seller/dashboard', function () {
+        //         return view('seller.index');
+        //     })->name('seller.dashboard');
+        // });
+
+        // Route::get('/seller/dashboard', function () {
+        //     return view('seller.index');
+        // })->name('seller.dashboard')->middleware(EnsureTokenIsValid::class);
+
+        // Route::get('/seller/test', function () {
+        //     return view('seller.test');
+        // })->middleware(CheckRole::class);
+
+
+
 
 require __DIR__.'/auth.php';
 
