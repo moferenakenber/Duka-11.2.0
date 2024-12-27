b9457c343bf985cab1a5449193376681f59f42e6
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-12-27 07:32:04 +0300

    o

--- resources/views/visitor/home.blade.php
+++ resources/views/visitor/home.blade.php
@@ -9,12 +9,12 @@
     <h1>Video Gallery</h1>
 
     <video controls width="600">
-        <source src="{{ asset('videos/Cars%20(2006)/Cars.2006.720p.BrRip.x264.YIFY.mp4') }}" type="video/mp4">
+        <source src="{{ asset('video/Cars%20(2006)/Cars.2006.720p.BrRip.x264.YIFY.mp4') }}" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 
     <video controls width="600">
-        <source src="{{ asset('videos/Cosmos.A.SpaceTime.Odyssey.S01.1080p.BluRay.x265-RARBG/Cosmos.A.SpaceTime.Odyssey.S01E04.1080p.BluRay.x265-RARBG.mp4') }}" type="video/mp4">
+        <source src="{{ asset('video/Cosmos.A.SpaceTime.Odyssey.S01.1080p.BluRay.x265-RARBG/Cosmos.A.SpaceTime.Odyssey.S01E04.1080p.BluRay.x265-RARBG.mp4') }}" type="video/mp4">
         Your browser does not support the video tag.
     </video>
 
