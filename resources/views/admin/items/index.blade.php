eaaaacacd07f8e2e504121ae220ecffeb0a8c097
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2025-09-29 01:01:09 +0300

    dashboard

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <div class="flex justify-between items-center">
+        <div class="flex items-center justify-between">
             <!-- Left side: Title -->
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                 {{ __('Items') }}
             </h2>
 
             <!-- Right side: Add Product Button -->
             <a href="{{ route('admin.items.create') }}"
-                class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                 {{ __('Add Item') }}
             </a>
         </div>
     </x-slot>
 
     @if ($errors->any())
-        <div class="mt-4 p-4 bg-red-100 border-l-4 border-red-500 text-red-700">
+        <div class="p-4 mt-4 text-red-700 bg-red-100 border-l-4 border-red-500">
             <h3 class="font-semibold">There were some problems with your input:</h3>
-            <ul class="list-disc pl-5 mt-2">
+            <ul class="pl-5 mt-2 list-disc">
                 @foreach ($errors->all() as $error)
                     <li class="text-sm">{{ $error }}</li>
                 @endforeach
@@ -27,18 +27,18 @@
     @endif
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white shadow-md rounded-lg overflow-hidden">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white rounded-lg shadow-md">
 
                 <div class="overflow-x-auto">
                     <table class="min-w-full table-auto">
                         <thead class="bg-gray-100">
                             <tr>
-                                <th class="px-6 py-2 text-left text-sm font-medium text-gray-900">Name</th>
-                                <th class="px-6 py-2 text-left text-sm font-medium text-gray-900">Price</th>
-                                <th class="px-6 py-2 text-left text-sm font-medium text-gray-900">Stock</th>
-                                <th class="px-6 py-2 text-left text-sm font-medium text-gray-900">Status</th>
-                                <th class="px-6 py-2 text-left text-sm font-medium text-gray-900">Actions</th>
+                                <th class="px-6 py-2 text-sm font-medium text-left text-gray-900">Name</th>
+                                <th class="px-6 py-2 text-sm font-medium text-left text-gray-900">Price</th>
+                                <th class="px-6 py-2 text-sm font-medium text-left text-gray-900">Stock</th>
+                                <th class="px-6 py-2 text-sm font-medium text-left text-gray-900">Status</th>
+                                <th class="px-6 py-2 text-sm font-medium text-left text-gray-900">Actions</th>
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200">
@@ -47,8 +47,9 @@
                                     <td class="px-6 py-2 text-sm text-gray-800">{{ $item->product_name }}</td>
                                     <td class="px-6 py-2 text-sm text-gray-800">{{ $item->price }}</td>
                                     <td class="px-6 py-2 text-sm text-gray-800">{{ $item->stock }}</td>
-                                    <td class="px-6 py-2 text-sm text-gray-800">{{ ucfirst($item->status) }}</td>
-                                    <td class="px-6 py-2 text-sm text-gray-800 flex items-center space-x-4">
+                                    {{-- <td class="px-6 py-2 text-sm text-gray-800">{{ ucfirst($item->status) }}</td> --}}
+                                    <td class="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-gray-300">{{ ucfirst($item->status) }}</td>
+                                    <td class="flex items-center px-6 py-2 space-x-4 text-sm text-gray-800">
 
                                         <!-- View Button -->
                                         <a href="{{ route('admin.items.show', $item->id) }}"
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -9,11 +9,24 @@
 
 
     <div class="py-12">
-        <div class="flex flex-col max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex-col bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="flex flex-col mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex-col overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <p>Welcome, {{ Auth::user()->first_name }}! You are now logged in.</p>
                     {{-- {{ __("You're logged in!") }} --}}
+
+                    <div class="flex flex-wrap gap-4 mt-4">
+                    <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white me-3"><span
+                            class="flex w-2.5 h-2.5 bg-blue-600 rounded-full me-1.5 shrink-0"></span>Visitors</span>
+                    <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white me-3"><span
+                            class="flex w-2.5 h-2.5 bg-purple-500 rounded-full me-1.5 shrink-0"></span>Sessions</span>
+                    <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white me-3"><span
+                            class="flex w-2.5 h-2.5 bg-indigo-500 rounded-full me-1.5 shrink-0"></span>Customers</span>
+                    <span class="flex items-center text-sm font-medium text-gray-900 dark:text-white me-3"><span
+                            class="flex w-2.5 h-2.5 bg-teal-500 rounded-full me-1.5 shrink-0"></span>Revenue</span>
+
+                    </div>
+
                 </div>
             </div>
         </div>
