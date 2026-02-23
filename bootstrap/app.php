dd915673b5c84a1f798c95c213558ea8e24ccd99
Yonathan <yonathan.hunetaw@gmail.com>
2026-02-23 12:45:39 +0300

    middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,26 +6,29 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\NotifyTelegramOnVisit;
+use App\Http\Middleware\LogVisitToDiscord;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // ✅ 1. Register Aliases
         $middleware->alias([
             'check_role' => CheckRole::class,
         ]);
-        // 👇 This is where you add your global middleware
 
-        $middleware->use([
-            NotifyTelegramOnVisit::class,
-        ]);// 👇 This is where you add your global middleware
-
+        $middleware->web(append: [
+            LogVisitToDiscord::class,
+            //NotifyTelegramOnVisit::class,
+        ]);
     })
+    ->withEvents(discover: [
+        __DIR__ . '/../app/Listeners', // This tells Laravel 11 to find your AlertLoginVisitor automatically!
+    ])
     ->withSchedule(function (Schedule $schedule) {
-        // ✅ Register your daily summary command at 9 PM here
         $schedule->command('visits:send-summary')->dailyAt('21:00');
     })
     ->withExceptions(function (Exceptions $exceptions) {
